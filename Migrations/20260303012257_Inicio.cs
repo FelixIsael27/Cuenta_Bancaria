e9b1668539a7a1b72cd0659926be7fa57bd11af1
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2026-03-02 21:24:37 -0400

    Añadiendo las migraciones

---
+++ Migrations/20260303012257_Inicio.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CuentaBancaria.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicio : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CuentasBancarias",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titular = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Numero_de_Cuenta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Saldo = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Tipo_de_Cuenta = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fecha_de_Creacion = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CuentasBancarias", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CuentasBancarias");
+        }
+    }
+}
