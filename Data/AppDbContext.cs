1256c303f50237ed5665583836325c087241f8fc
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2026-03-02 21:07:42 -0400

    Agregar archivos de proyecto.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CuentaBancaria.Models;
+
+namespace CuentaBancaria.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Datos_Cuenta> CuentasBancarias { get; set; }
+    }
+}
---
+++ Models/Datos_Cuenta.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CuentaBancaria.Models
+{
+    public class Datos_Cuenta
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Titular { get; set; }
+
+        [Required]
+        public string Numero_de_Cuenta { get; set; }
+
+        [Range(0, double.MaxValue)]
+        public decimal Saldo { get; set; }
+
+        [Required]
+        public string Tipo_de_Cuenta { get; set; }
+
+        public DateTime Fecha_de_Creacion { get; set; } = DateTime.Now;
+    }
+}
